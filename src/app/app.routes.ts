3c3c9577cc4785e0a65820fbdbc32ea854e70315
Markus Vickman~ <markus.vickman@hotmail.com>
2024-04-12 12:57:49 +0200

    first commit first angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConvertComponent } from './convert/convert.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'covert', component: ConvertComponent},
+    {path: 'about', component: AboutComponent},
+    {path: '', component: HomeComponent},
+    {path: '**', component: NotFoundComponent}
+];
